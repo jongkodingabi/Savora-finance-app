5d32220592f4ed725bf9cacbee75e4b13faff01a
jongkodingabi <abisamhazim04@gmail.com>
2025-07-12 11:17:53 +0700

    fixing some layout

--- src/pages/Landing/Home.tsx
+++ src/pages/Landing/Home.tsx
@@ -8,7 +8,12 @@ import Footer from "@/components/ui/footer";
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-zinc-950 text-white">
-      <div className="absolute -top-8 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-emerald-400 via-cyan-400 to-blue-400 rounded-full w-[700px] h-[200px] blur-3xl z-10 overflow-x-hidden"></div>
+      <div
+        className="absolute -top-8 left-1/2 -translate-x-1/2 -translate-y-1/2
+             bg-gradient-to-r from-emerald-400 via-cyan-400 to-blue-400
+             rounded-full blur-3xl z-10 overflow-x-hidden
+             w-[90%] h-[120px] sm:h-[150px] md:h-[180px] lg:h-[200px] max-w-[700px]"
+      ></div>
       {/* Header */}
       <HeaderNavigation />
 
